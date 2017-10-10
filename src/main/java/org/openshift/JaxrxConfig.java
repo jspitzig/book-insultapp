5516512733ad8ecea8a5f58c6f8b29703c6ed158
Spitzig, Jeremy <jspitzig@cardinalsolutions.com>
2017-10-10 19:42:01 -0400

    Add API

---
+++ src/main/java/org/openshift/JaxrxConfig.java
@@ -0,0 +1,11 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by jspitzig on 10/10/17.
+ */
+@ApplicationPath("/api")
+public class JaxrxConfig extends Application {
+}
